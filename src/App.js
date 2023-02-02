fdf975baf74b2042dba79c4e4e139e92a18c1133
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2023-02-02 21:42:57 +0530

    Creating the menu page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu';
+import { useState } from 'react';
 
 function App() {
+  const [gameState, setGameState] = useState('Menu');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz App</h1>
+      {gameState === 'Menu' && <Menu />}
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+const Menu = () => {
+  return (
+    <div className="Menu">
+      <label>Enter Your Name</label>
+      <input type="text" placeholder="Ex." />
+      <button>Start Quiz</button>
+    </div>
+  );
+};
+
+export default Menu;
